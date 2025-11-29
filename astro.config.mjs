e15cb2e912eff4a227b800c14fc9db8eca779127
Cifhe <yjsnkssunday@outlook.com>
2025-11-29 22:47:18 +0800

    update astro site config and site files

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://Laocaiyj.github.io.git',
+	site: 'https://Laocaiyj.github.io',
 	base: '/',
 	integrations: [mdx(), sitemap()],
 });
